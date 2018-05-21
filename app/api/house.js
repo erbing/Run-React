67c531f3161feb473458a33ee94976890f191481
SmallW <447587073@qq.com>
2018-05-21 14:40:54 +0800

    change lib

--- app/api/house.js
+++
@@ -1,4 +0,0 @@
-import { ajax } from 'utils'
-
-export const houseCheckList = ajax.fetchJSONByPost('/api/demo')
-export const houseDetail = ajax.fetchJSONByPost('/house/detail')
--- app/pages/house/index.js
+++
@@ -1,8 +0,0 @@
-import houseCheck from './houseCheck'
-import houseManage from './houseManage'
-import houseDetail from './houseDetail'
-import roomDetail from './roomDetail'
-
-export {
-    houseCheck, houseManage, houseDetail, roomDetail,
-}
