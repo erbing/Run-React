acd42242dc36ccfeb517bf4fc5f9566147910078
SmallW <447587073@qq.com>
2018-05-23 17:12:13 +0800

    add index logo

--- app/modules/slide.js
+++ app/modules/slide.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const logoImg = require('../assets/img/index/logo.jpg')
+// const logoImg = 'xxx'
+
 export default class Header extends React.Component {
     constructor(props) {
         super(props)
@@ -9,6 +12,10 @@ export default class Header extends React.Component {
     render() {
         return (
             <div className="slide">
+                <div className="slide-header flex-xy">
+                    <img src={logoImg}></img>
+                </div>
+                
                 <ul>
                     <li> <Link to={'/'}> home </Link></li>
                     <li> <Link to={'/index'}>index </Link></li>
