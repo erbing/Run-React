146ebe124fb3e27f78429564bc74668e5e6e8dc1
SmallW <447587073@qq.com>
2018-11-08 16:14:05 +0800

    change

--- app/modules/slide.js
+++ app/modules/slide.js
@@ -1,33 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Switch, Link, Route } from "react-router-dom";
 
-const logoImg = require('../assets/img/index/logo2.jpg')
+const logoImg = require("../assets/img/index/logo2.jpg");
 
 export default class Header extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="slide">
-                <div className="slide-header flex-xy">
-                    <img src={logoImg} className="slide-logo"></img>
-                    <span className="slogn">ReactJs</span>
-                    <em className="beta">Beta</em>
-                </div>
-                
-                <ul className="slide-ul">
-                    <li className="slide-li"> <Link to={'/'}> HOME </Link></li>
-                    <li className="slide-li"> <Link to={'/index'}>INDEX </Link></li>
-                    <li className="slide-li"> <Link to={'/my'}>MY </Link></li>
-                    <li className="slide-li"> <Link to={'/icon'}>ICON </Link></li>
-                    <li className="slide-li"> <Link to={'/http'}>HTTP</Link></li>
-                    <li className="slide-li"> <Link to={'/button'}>BUTTON</Link></li>
-                    <li className="slide-li"> <Link to={'/about'}>ABOUT</Link></li>
-                    <li className="slide-li"> <Link to={'/lazyLoad'}>Lazy Load</Link></li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="slide">
+        <div className="slide-header flex-xy">
+          <img src={logoImg} className="slide-logo" />
+          <span className="slogn">ReactJs</span>
+          <em className="beta">Beta</em>
+        </div>
+
+        <ul className="slide-ul">
+          <li className="slide-li">
+            <Link to={"/"}> HOME </Link>
+          </li>
+          <li className="slide-li">
+            <Link to={"/index"}>INDEX </Link>
+          </li>
+          <li className="slide-li">
+            <Link to={"/my"}>MY </Link>
+          </li>
+          <li className="slide-li">
+            <Link to={"/icon"}>ICON </Link>
+          </li>
+          <li className="slide-li">
+            <Link to={"/http"}>HTTP</Link>
+          </li>
+          <li className="slide-li">
+            <Link to={"/button"}>BUTTON</Link>
+          </li>
+          <li className="slide-li">
+            <Link to={"/about"}>ABOUT</Link>
+          </li>
+          <li className="slide-li">
+            <Link to={"/lazyLoad"}>Lazy Load</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
