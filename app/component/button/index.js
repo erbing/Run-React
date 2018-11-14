47def4b9876001a6c61e878dd86f1d72b5b11fda
SmallW <447587073@qq.com>
2018-11-14 14:37:56 +0800

    add sime btn comp show

--- app/component/button/index.js
+++ app/component/button/index.js
@@ -7,10 +7,32 @@ class Button extends React.Component {
     super(props);
   }
   render() {
-    let { type, text } = this.props;
+    let { type, text, display, disabled, radius } = this.props;
+    let isPercent = radius ? radius.indexOf("%") > -1 : false;
 
+    let styles = {
+      borderRadius: isPercent ? radius : radius + "px"
+    };
+    // 特殊处理 圆形
+    if (isPercent && radius.replace("%", "") > 49) {
+      styles = {
+        borderRadius: "50%",
+        padding: "16px",
+        width: "44px"
+      };
+    }
+    console.log(styles, 123);
     return (
-      <a className={"run-button " + "run-button-" + (type ? type : "default")}>
+      <a
+        className={
+          "run-button " +
+          "run-button-" +
+          (type ? type : "default") +
+          (display == "block" ? " display" : "") +
+          (disabled ? " disabled" : " ")
+        }
+        style={styles}
+      >
         <span>{text}</span>
       </a>
     );
--- app/pages/button/index.js
+++ app/pages/button/index.js
@@ -8,15 +8,33 @@ const ButtonApp = props => {
   return (
     <div>
       <Box>
+        <BoxTitle title="基本用法" />
+        <Button text="default" />
+        <Button type="primary" text="primary" />
+
+        <BoxTitle title="块级按钮" />
+        <Button text="default" display="block" />
+        <Button text="default disabled" display="block" disabled />
+        <Button type="primary" text="primary" display="block" />
+        <Button
+          type="primary"
+          text="primary  disabled"
+          display="block"
+          disabled
+        />
+
         <BoxTitle title="按钮主题" />
         <Button text="default" />
         <Button type="primary" text="primary" />
         <Button type="primary" text="success" />
         <Button type="warning" text="warning" />
         <Button type="error" text="error" />
-        <BoxTitle title="基本用法" />
-        <Button text="default" />
-        <Button type="primary" text="primary" />
+
+        <BoxTitle title="按钮形状" />
+        <Button type="primary" text="rect" />
+        <Button type="primary" text="radius" radius="4" />
+        <Button type="primary" text="round" radius="25" />
+        <Button type="primary" text="circle" radius="50%" />
       </Box>
     </div>
   );
