9491b1590108d4b6ebaaf66d8594ea3ced78f0c2
SmallW <447587073@qq.com>
2018-11-21 10:34:33 +0800

    change comp

--- app/component/modal/index.js
+++ app/component/modal/index.js
@@ -2,33 +2,52 @@ import React from "react";
 import Mast from "@component/mask";
 import "./index.less";
 
-const Modal = props => {
-  let { title, content, visible } = props;
-  let maskStyle = {
-    width: "70%",
-    animationDuration: "200ms"
-  };
-  const close = () => {
-    alert(111);
-    visible = false;
-  };
-  return (
-    <div className="run-modal">
-      <div className="run-modal-wrapper">
-        <div className="run-modal-dialog" style={maskStyle}>
-          <div className="run-modal-header">
-            <div className="run-modal-header-title">{title}</div>
-            <div className="run-modal-header-close" onClick={close}>
-              <i className="icon-close_light icon" />
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alert: false,
+      confirm: false
+    };
+  }
+
+  close() {
+    console.log(123);
+    this.setState({
+      visible: false
+    });
+  }
+
+  render() {
+    let { title, content, visible } = this.props;
+    let maskStyle = {
+      width: "70%",
+      animationDuration: "200ms"
+    };
+
+    return (
+      visible && (
+        <div className="run-modal">
+          <div className="run-modal-wrapper">
+            <div className="run-modal-dialog" style={maskStyle}>
+              <div className="run-modal-header">
+                <div className="run-modal-header-title">{title}</div>
+                <div
+                  className="run-modal-header-close"
+                  onClick={() => this.close()}
+                >
+                  <i className="icon-close_light icon" />
+                </div>
+              </div>
+              <div className="run-modal-content">{content}</div>
+              {/* <div className="run-modal-confirm">确定</div> */}
             </div>
           </div>
-          <div className="run-modal-content">{content}</div>
-          {/* <div className="run-modal-confirm">确定</div> */}
+          <Mast />
         </div>
-      </div>
-      <Mast />
-    </div>
-  );
-};
+      )
+    );
+  }
+}
 
 export default Modal;
