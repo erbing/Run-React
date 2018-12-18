8d33e6df4b46095cac18e2e73d7159d0576d5e17
SmallW <447587073@qq.com>
2018-12-18 21:54:02 +0800

    add input comp

--- app/component/input/index.js
+++ app/component/input/index.js
@@ -1,3 +1,12 @@
 import React from "react";
+import "./index.less";
 
-export default class Input extends React.Component {}
+export default class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>234</div>;
+  }
+}
