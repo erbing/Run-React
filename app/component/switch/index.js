0a2b0e34b8e9193311a59bc379cfcc6a0d1c6943
SmallW <447587073@qq.com>
2018-12-26 00:35:09 +0800

    add switch base comp

--- app/component/switch/index.js
+++ app/component/switch/index.js
@@ -1,13 +1,40 @@
 import React from "react";
 import "./index.less";
 
-const Switch = props => {
-  let { title } = props;
-  return (
-    <span>
-      <input type="checkbox" className="switch-input" />
-    </span>
-  );
-};
+class Switch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false
+    };
+  }
+
+  onValueChange = () => {
+    const { disabled, onChange } = this.props;
+    if (disabled) {
+      return;
+    }
+
+    const checked = !this.state.checked;
+    this.setState({ checked });
+
+    if (typeof onChange === "function") {
+      onChange(checked);
+    }
+  };
+
+  render() {
+    return (
+      <span className="run-switch">
+        <input
+          type="checkbox"
+          className="switch-input"
+          value=""
+          onChange={this.onValueChange}
+        />
+      </span>
+    );
+  }
+}
 
 export default Switch;
--- app/pages/switch/index.js
+++ app/pages/switch/index.js
@@ -3,18 +3,30 @@ import React from "react";
 import Switch from "@component/switch";
 import Box from "@component/box";
 import BoxTitle from "@component/boxTitle";
+import Item from "@component/item";
 
 const SwitchApp = props => {
-  let curStyle = {
-    marginLeft: "10px"
+  let state = {
+    checked: false
   };
+
+  const handleCheck = value => {
+    console.log(value);
+    state.checked = value;
+  };
+
   return (
     <div>
       <Box title="开关 Switch">
         <BoxTitle title="基本用法" />
-        <div style={curStyle}>
-          <Switch />
-        </div>
+        <Item text="普通">
+          <Switch
+            checked={state.checked}
+            onChange={v => {
+              handleCheck(v);
+            }}
+          />
+        </Item>
       </Box>
     </div>
   );
