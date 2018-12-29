b79d76d89f50107b00ca91b5c88b83c1fd3f8c96
SmallW <447587073@qq.com>
2018-12-29 23:37:03 +0800

    add popup

---
+++ app/component/popup/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.less";
+
+class Popup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <div>123</div>
+      </div>
+    );
+  }
+}
