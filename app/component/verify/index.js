f1785159cec9de12a7b000fdb4cb1ca2d8430fb0
SmallW <447587073@qq.com>
2019-01-16 18:04:05 +0800

    change

--- app/component/verify/index.js
+++ app/component/verify/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.less";
 
 const imgPath =
-  "https://img2018.cnblogs.com/blog/675289/201901/675289-20190116164128915-1434002987.jpg";
+  "https://img2018.cnblogs.com/blog/675289/201901/675289-20190116165247535-2019476226.jpg";
 
 class newCanvas {
   constructor(options) {
@@ -15,7 +15,14 @@ class Verify extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      type: 1
+      type: 1,
+      startX: 0,
+      moveX: 0,
+      endX: 0,
+      leftStyle: {
+        left: 10
+      },
+      lefts: 10
     };
   }
 
@@ -24,15 +31,62 @@ class Verify extends React.Component {
       <div>
         <img src={imgPath} className="show-img" />
         <canvas id="verify" className="verify-canvas" />
+        <div className="sliding">
+          <div
+            className="sliding-btn flex-xy"
+            id="moveBtn"
+            style={{ left: this.state.lefts + "px" }}
+          >
+            <i className={"icon-right icon sliding-btn-icon"} />
+          </div>
+        </div>
       </div>
     );
   }
+
+  moveBtn() {
+    let moveBtnDom = document.getElementById("moveBtn");
+
+    moveBtnDom.onmousedown = e => {
+      this.state.startX = e.clientX;
+      console.log(e.clientX, "---start---");
+    };
+
+    moveBtnDom.onmousemove = e => {
+      if (this.state.startX) {
+        this.state.moveX = e.clientX - this.state.startX;
+        console.log(this.state.moveX);
+        if (this.state.moveX < 0) {
+          this.state.moveX = 0;
+        }
+        this.setState({
+          lefts: this.state.moveX
+        });
+        // this.state.leftStyle.left = this.state.moveX + "px";
+      }
+    };
+
+    moveBtnDom.onmouseup = e => {
+      if (this.state.startX) {
+        console.log(e.clientX, "---end---");
+        this.state.startX = 0;
+      }
+    };
+  }
+
+  moveStart(e) {}
+
+  moveing() {}
+
+  moveEnd() {}
 
   componentDidMount() {
     let cavs = document.getElementById("verify");
     cavs.width = 400;
     cavs.height = 200;
     let drawArea = cavs.getContext("2d");
+
+    this.moveBtn();
 
     let imgs = new Image();
     imgs.src = imgPath;
