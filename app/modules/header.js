62cc816cde6993b7504fced4d89b2c0f12ebc6cc
SmallW <447587073@qq.com>
2019-01-17 10:40:05 +0800

    add github  logo

--- app/modules/header.js
+++ app/modules/header.js
@@ -3,16 +3,30 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
+const imgGithub = require("./images/github.png");
+
 class Header extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  toGithub() {
+    window.location.href = "https://github.com/erbing/Run-React";
+  }
+
   render() {
     let { title } = this.props;
     return (
-      <div className="main-header">
+      <div className="main-header flex-y flex-between">
         <span className="header-title">{title}</span>
+        <img
+          src={imgGithub}
+          alt="github"
+          className="header-github"
+          onClick={() => {
+            this.toGithub();
+          }}
+        />
       </div>
     );
   }
